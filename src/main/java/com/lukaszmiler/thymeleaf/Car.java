f983b46cfc644027e7c9131f65007c677bcd6f78
lmiler <lukasz.miler@gmail.com>
2022-11-09 13:01:46 +0100

    done

--- src/main/java/com/lukaszmiler/thymeleaf/Car.java
+++ src/main/java/com/lukaszmiler/thymeleaf/Car.java
@@ -1,2 +1,22 @@
-package com.lukaszmiler.thymeleaf;public class Car {
+package com.lukaszmiler.thymeleaf;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Car {
+    private int id;
+    private String mark;
+    private String model;
+    private String color;
+
+    public static boolean equals(Car a, Car b){
+        return a.getId() == b.getId() && a.getMark().equalsIgnoreCase(b.getMark()) && a.getModel().equalsIgnoreCase(b.getModel()) && a.getColor().equalsIgnoreCase(b.getColor());
+    }
 }
+
+
+
--- src/main/java/com/lukaszmiler/thymeleaf/CarController.java
+++ src/main/java/com/lukaszmiler/thymeleaf/CarController.java
@@ -2,16 +2,59 @@ package com.lukaszmiler.thymeleaf;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
-public class CarShow {
+public class CarController {
 
+    List<Car> carList = new ArrayList<>();
 
-    @GetMapping("/hello")
-    String hello(Model model){
-        model.addAttribute("car", new Car(1, "Toyota", "Yaris", "white"));
-        return "hello";
+    public CarController() {
+        carList.add(new Car(1, "Toyota", "Yaris", "White"));
+        carList.add(new Car(2, "Audi", "Q8", "Red"));
+        carList.add(new Car(3, "Skoda", "Superb", "Blue"));
+        carList.add(new Car(4, "Fiat", "Sienna", "Blue"));
+    }
+
+    @GetMapping("/car")
+    public String showCars(Model model) {
+        model.addAttribute("cars", carList);
+        model.addAttribute("newCar", new Car());
+        return "car";
+    }
+
+    @PostMapping("/addCar")
+    public String addCar(@ModelAttribute Car car) {
+        carList.add(car);
+        return "redirect:/car";
+    }
+
+    @GetMapping("/removeCar")
+    public String removeCar(@ModelAttribute Car car) {
+        for (Car c : carList) {
+            if (c.getId() == car.getId()) {
+                carList.remove(c);
+                break;
+            }
+        }
+        return "redirect:/car";
+    }
+
+    @GetMapping("/modifyCar")
+    public String modifyCar(@ModelAttribute Car car) {
+        for (Car c : carList) {
+            if (c.getId() == car.getId()) {
+                c.setId(car.getId());
+                c.setModel(car.getModel());
+                c.setMark(car.getMark());
+                c.setColor(car.getColor());
+                break;
+            }
+        }
+        return "redirect:/car";
     }
 
 }
